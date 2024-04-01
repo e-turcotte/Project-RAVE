0ad8dbdcc30e89aa8fae6ecc77521d399cabaddd
rohanjainUTENGR <rohanjain@utexas.edu>
2024-03-31 22:37:16 -0500

    update prefix decoding

--- processor/D/prefix_d.v
+++ processor/D/prefix_d.v
@@ -1,22 +1,50 @@
 module prefix_d (
     input clk,
     input reset,
-    input [127:0] instruction,
+    input [127:0] packet,
     output [1:0] num_prefixes,
     output is_rep,
     output is_seg_override,
     output is_opsiz_override;
-    );
+    ); //TODO: how to encode which seg override - there are 6 possible values
 
     wire [7:0] prefix1, prefix2, prefix3;
 
-    buffer8$(instruction[127:120], prefix1) //high byte
-    buffer8$(instruction[119:112], prefix2) //middle byte
-    buffer8$(instruction[111:104], prefix3) //low byte
+    assign prefix1 = packet[127:120];
+    assign prefix2 = packet[119:112];
+    assign prefix3 = packet[111:104];
 
-    //compare each prefix to the known prefixes in lookup table
+    wire is_rep1, is_rep2, is_rep3;
+    wire is_seg_override1, is_seg_override2, is_seg_override3;
+    wire is_opsize_override1, is_opsize_override2, is_opsize_override3;
     
-    //do a  128b mux for comparison results
-    
+    prefix_lut prefix_lut1(
+        .prefix(prefix1),
+        .is_rep(is_rep1),
+        .is_seg_override(is_seg_override1),
+        .is_opsize_override(is_opsiz_override1)
+    );
+
+    prefix_lut prefix_lut2(
+        .prefix(prefix2),
+        .is_rep(is_rep2),
+        .is_seg_override(is_seg_override2),
+        .is_opsize_override(is_opsiz_override2)
+    );
+
+    prefix_lut prefix_lut3(
+        .prefix(prefix3),
+        .is_rep(is_rep3),
+        .is_seg_override(is_seg_override3),
+        .is_opsize_override(is_opsiz_override3)
+    );
+
+    nand3$(is_rep1, is_rep2, is_rep3, is_rep);
+    nand3$(is_seg_override1, is_seg_override2, is_seg_override3, is_seg_override);
+    nand3$(is_opsize_override1, is_opsize_override2, is_opsize_override3, is_opsize_override);
+
+    //count the number of prefixes
+    //TODO: how to encode num prefixes: onehot?
+
 
 endmodule
--- processor/D/prefix_lut.v
+++ processor/D/prefix_lut.v
@@ -1,16 +1,38 @@
+ 
+
 module prefix_lut(
-    input [7:0] prefix1,
-    input [7:0] prefix2,
-    input [7:0] prefix3,
-    output [1:0] num_prefixes,
+    input clk,
+    input reset,
+    input [7:0] prefix,
     output is_rep,
     output is_seg_override,
-    output is_opsiz_override
-    );
+    output is_opsize_override;
+    ); //TODO: how to encode which seg override?
 
     //compare each prefix to the known prefixes in lookup table
     
+    wire [7:0] rep, seg_cs, seg_ss, seg_ds, seg_es, seg_fs, seg_gs, opsize;
+    assign rep = 8'hF3;
+    assign seg_cs = 8'h2E;
+    assign seg_ss = 8'h36;
+    assign seg_ds = 8'h3E;
+    assign seg_es = 8'h26;
+    assign seg_fs = 8'h64;
+    assign seg_gs = 8'h65;
+    assign opsize = 8'h66;
+
+    //TODO: do I need to add a buffer for fanout?
+    //magic_comp8 will be 0 if they are equal
+    mag_comp8$(prefix, rep, is_rep);
+    mag_comp8$(prefix, seg_cs, is_seg_override);
+    mag_comp8$(prefix, seg_ss, is_seg_override);
+    mag_comp8$(prefix, seg_ds, is_seg_override);
+    mag_comp8$(prefix, seg_es, is_seg_override);
+    mag_comp8$(prefix, seg_fs, is_seg_override);
+    mag_comp8$(prefix, seg_gs, is_seg_override);
+    mag_comp8$(prefix, opsize, is_opsize_override);
     
+
 
 
 endmodule
