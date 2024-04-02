0ba8ee68d5d1f624fd1425d1b48de2d7656808bb
rohanjainUTENGR <rohanjain@utexas.edu>
2024-04-02 16:47:55 -0500

    explicitly declare parameter names in module insta ntiation

--- local-lib/bufferH16_8b$.v
+++ local-lib/bufferH16_8b$.v
@@ -3,13 +3,13 @@ module bufferH16_8b$(
     output [7:0] out
     );
 
-bufferH16$ b0(out[0], in[0]);
-bufferH16$ b1(out[1], in[1]);
-bufferH16$ b2(out[2], in[2]);
-bufferH16$ b3(out[3], in[3]);
-bufferH16$ b4(out[4], in[4]);
-bufferH16$ b5(out[5], in[5]);
-bufferH16$ b6(out[6], in[6]);
-bufferH16$ b7(out[7], in[7]);
+bufferH16$ b0(.out(out[0]), .in(in[0]));
+bufferH16$ b1(.out(out[1]), .in(in[1]));
+bufferH16$ b2(.out(out[2]), .in(in[2]));
+bufferH16$ b3(.out(out[3]), .in(in[3]));
+bufferH16$ b4(.out(out[4]), .in(in[4]));
+bufferH16$ b5(.out(out[5]), .in(in[5]));
+bufferH16$ b6(.out(out[6]), .in(in[6]));
+bufferH16$ b7(.out(out[7]), .in(in[7]));
 
 endmodule
