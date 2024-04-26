89550eb645404b3464f64387b59dc424d905028c
rohanjainUTENGR <rohanjain@utexas.edu>
2024-04-25 23:44:04 -0500

    add onhot encoding of num prefixes to prefix_d.v - adds some delay

--- processor/D/prefix_d.v
+++ processor/D/prefix_d.v
@@ -4,9 +4,16 @@ module prefix_d (
     output [5:0] seg_override, //onehot
     output is_seg_override,
     output is_opsize_override,
-    output [1:0] num_prefixes //sum of all prefixes (not onehot)
+    output [1:0] num_prefixes_encoded. //sum of all prefixes (not onehot)
+    output [3:0] num_prefixes_onehot //onehot encoding of num_prefixes
     );
-    //total prefix decode time is 3.35ns (i think)
+//num_prefixes_onehot
+    //0001: 0 prefixes
+    //0010: 1 prefix
+    //0100: 2 prefixes
+    //1000: 3 prefixes
+
+    //total prefix decode time is 2.25ns (i think)
     wire [7:0] prefix1, prefix2, prefix3;
 
     assign prefix1 = packet[127:120];
@@ -60,9 +67,11 @@ module prefix_d (
     nand3$(.out(is_seg_override), .in0(nor_1), .in1(nor_2), .in2(nor_3)); //0.2ns
 
     //to encode num prefixes as a sum:
-
     fulladder1$ fa(.A(is_rep), .B(is_seg_override), .cin(is_opsize_override),
-                   .sum(num_prefixes[0]), .cout(num_prefixes[1])); //0.7ns
+                   .sum(num_prefixes_encoded[0]), .cout(num_prefixes_encoded[1])); //0.7ns
+
+
+    decodern #(.INPUT_WIDTH(2)) decodern1(.in(num_prefixes_encoded), .out(num_prefixes_onehot));
 
 endmodule
 
@@ -78,15 +87,6 @@ module prefix_cmp(
 
     wire [7:0] rep, seg_cs, seg_ss, seg_ds, seg_es, seg_fs, seg_gs, opsize;
     wire [7:0] prefix_temp;
-
-    // wire rep_gt, rep_lt;
-    // wire seg_cs_gt, seg_cs_lt;
-    // wire seg_ss_gt, seg_ss_lt;
-    // wire seg_ds_gt, seg_ds_lt;
-    // wire seg_es_gt, seg_es_lt;
-    // wire seg_fs_gt, seg_fs_lt;
-    // wire seg_gs_gt, seg_gs_lt;
-    // wire opsize_gt, opsize_lt;
     
     //hardcode known prefixes
     assign rep = 8'hF3;
@@ -101,19 +101,6 @@ module prefix_cmp(
     //need to buffer for fanout
     bufferH16_8b$ b0(prefix, prefix_temp); //0.24ns
 
-    //TODO: do I need to add a buffer to prefix for fanout? - probably
-
-    //compare each prefix to the known prefixes
-    //output will be 0 if they are equal (ie we have a match)
-    // mag_comp8$(prefix_temp, rep, rep_gt, rep_lt); //1.46ns in parallel
-    // mag_comp8$(prefix_temp, seg_cs, seg_cs_gt, seg_cs_lt);
-    // mag_comp8$(prefix_temp, seg_ss, seg_ss_gt, seg_ss_lt);
-    // mag_comp8$(prefix_temp, seg_ds, seg_ds_gt, seg_ds_lt);
-    // mag_comp8$(prefix_temp, seg_es, seg_es_gt, seg_es_lt);
-    // mag_comp8$(prefix_temp, seg_fs, seg_fs_gt, seg_fs_lt);
-    // mag_comp8$(prefix_temp, seg_gs, seg_gs_gt, seg_gs_lt);
-    // mag_comp8$(prefix_temp, opsize, opsize_gt, opsize_lt);
-
     equaln #(.WIDTH(8)) rep_mux(.a(rep), .b(prefix_temp), .eq(is_rep)); //0.7ns in parallel
     equaln #(.WIDTH(8)) seg_cs_mux(.a(seg_cs), .b(prefix_temp), .eq(seg_override[0]));
     equaln #(.WIDTH(8)) seg_ss_mux(.a(seg_ss), .b(prefix_temp), .eq(seg_override[1]));
@@ -122,16 +109,6 @@ module prefix_cmp(
     equaln #(.WIDTH(8)) seg_fs_mux(.a(seg_fs), .b(prefix_temp), .eq(seg_override[4]));
     equaln #(.WIDTH(8)) seg_gs_mux(.a(seg_gs), .b(prefix_temp), .eq(seg_override[5]));
     equaln #(.WIDTH(8)) opsize_mux(.a(opsize), .b(prefix_temp), .eq(is_opsize_override));
-
-    // nor2$(is_rep, rep_gt, rep_lt); //0.2 ns in parallel
-    // nor2$(seg_override[0], seg_cs_gt, seg_cs_lt);
-    // nor2$(seg_override[1], seg_ss_gt, seg_ss_lt);
-    // nor2$(seg_override[2], seg_ds_gt, seg_ds_lt);
-    // nor2$(seg_override[3], seg_es_gt, seg_es_lt);
-    // nor2$(seg_override[4], seg_fs_gt, seg_fs_lt);
-    // nor2$(seg_override[5], seg_gs_gt, seg_gs_lt);
-    // nor2$(is_opsize_override, opsize_gt, opsize_lt);
-    //at this point, outputs should be 1 for true (since nand)
 
 endmodule
 
