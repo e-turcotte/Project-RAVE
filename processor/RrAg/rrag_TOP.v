55a63da43d67ae1b3fcf04a2184b85a1e0dc48d8
e-turcotte <eturcotte@utexas.edu>
2024-04-26 22:23:00 -0500

    Touch up RrAg

--- processor/RrAg/rrag_TOP.v
+++ processor/RrAg/rrag_TOP.v
@@ -5,11 +5,8 @@ module rrag (input valid_in,
              input res1_ld, res2_ld, res3_ld, res4_ld,
              input [12:0] dest1_in, dest2_in, dest3_in, dest4_in,
              input [31:0] disp,
-             input [1:0] ss,
-             input is_sib,
-             input seg_switch,
-             input [2:0] seg_switch_to,
-             input reg_or_disp,
+             input [1:0] reg3_shfamnt,
+             input usereg2, usereg3,
              input rep,
 
              input clr, clk,
@@ -32,7 +29,7 @@ module rrag (input valid_in,
 
              input [63:0] wb_data1, wb_data2, wb_data3, wb_data4,
              input [15:0] wb_segdata1, wb_segdata2, wb_segdata3, wb_segdata4,
-             input [2:0] wb_addr1, wb_addr2, wb_addr3, wb_addr4, wb_segaddr,
+             input [2:0] wb_addr1, wb_addr2, wb_addr3, wb_addr4, wb_segaddr1, wb_segaddr2, wb_segaddr3, wb_segaddr4,
              input [1:0] wb_opsize,
              input [3:0] wb_regld, wb_segld,
              input [6:0] wb_inst_ptcid,
@@ -45,8 +42,9 @@ module rrag (input valid_in,
              output [127:0] ptc_r1, ptc_r2, ptc_r3, ptc_r4,
              output [2:0] reg1_orig, reg2_orig, reg3_orig, reg4_orig,
              output [15:0] seg1, seg2, seg3, seg4,
-             output [15:0] ptc_s1, ptc_s2, ptc_s3, ptc_s4,
+             output [31:0] ptc_s1, ptc_s2, ptc_s3, ptc_s4,
              output [6:0] inst_ptcid,
+             output [12:0] op1_out, op2_out, op3_out, op4_out,
              output [12:0] dest1_out, dest2_out, dest3_out, dest4_out,
              output [31:0] rep_num,
              
@@ -83,7 +81,7 @@ module rrag (input valid_in,
     wire [19:0] lim_out1, lim_out2, lim_out3, lim_out4;
 
     regfile rf(.din({wb_data4,wb_data3,wb_data2,wb_data1}), .ld_addr({wb_addr4,wb_addr3,wb_addr2,wb_addr1}), .rd_addr({reg_addr4,reg_addr3,reg_addr2,reg_addr1}), .ldsize(wb_opsize), .rdsize(opsize_in), .ld_en(wb_regld), .dest(collated_dest_vector[7:4]), .data_ptcid(wb_inst_ptcid), .new_ptcid(inst_ptcid), .clr(clr), .clk(clk), .dout({reg4,reg3,reg2,reg1}), .ptcout({ptc_r4,ptc_r3,ptc_r2,ptc_r1}));
-    segfile sf(.base_in(wb_segdata1), .lim_inits({lim_init5,lim_init4,lim_init3,lim_init2,lim_init1,lim_init0}), .ld_addr({wb_segaddr4,wb_segdata3,wb_segdata2,wb_segdata1}), .rd_addr({seg_addr4,seg_addr3,seg_addr2,seg_addr1}), .ld_en(wb_segld), .dest(collated_dest_vector[3:0]), .data_ptcid(wb_inst_ptcid), .new_ptcid(inst_ptcid), .clr(clr), .clk(clk), .base_out({seg4,seg3,seg2,seg1}), .lim_out({lim_out4,lim_out3,lim_out2,lim_out1}), .ptc_out({ptc_s4,ptc_s3,ptc_s2,ptc_s1}));
+    segfile sf(.base_in({wb_segdata4,wb_segdata3,wb_segdata2,wb_segdata1}), .lim_inits({lim_init5,lim_init4,lim_init3,lim_init2,lim_init1,lim_init0}), .ld_addr({wb_segaddr4,wb_segaddr3,wb_segaddr2,wb_segaddr1}), .rd_addr({seg_addr4,seg_addr3,seg_addr2,seg_addr1}), .ld_en(wb_segld), .dest(collated_dest_vector[3:0]), .data_ptcid(wb_inst_ptcid), .new_ptcid(inst_ptcid), .clr(clr), .clk(clk), .base_out({seg4,seg3,seg2,seg1}), .lim_out({lim_out4,lim_out3,lim_out2,lim_out1}), .ptc_out({ptc_s4,ptc_s3,ptc_s2,ptc_s1}));
 
     wire [31:0] shfseg1, shfseg2;
 
@@ -91,19 +89,19 @@ module rrag (input valid_in,
     assign shfseg2 = {seg2,16'h0000};
 
     wire [31:0] mul2, mul4, mul8;
-    wire [31:0] shfidx;
+    wire [31:0] shfreg3;
 
     lshfn_fixed #(.WIDTH(32), .SHF_AMNT(1)) s0(.in(reg3[31:0]), .shf_val(1'b0), .out(mul2));
     lshfn_fixed #(.WIDTH(32), .SHF_AMNT(2)) s1(.in(reg3[31:0]), .shf_val(2'b00), .out(mul4));
     lshfn_fixed #(.WIDTH(32), .SHF_AMNT(3)) s2(.in(reg3[31:0]), .shf_val(3'b000), .out(mul8));
-    muxnm_tree #(.SEL_WIDTH(2), .DATA_WIDTH(32)) m0(.in({mul8,mul4,mul2,reg3[31:0]}), .sel(ss), .out(shfidx));
+    muxnm_tree #(.SEL_WIDTH(2), .DATA_WIDTH(32)) m0(.in({mul8,mul4,mul2,reg3[31:0]}), .sel(reg3_shfamnt), .out(shfreg3));
 
-    wire [31:0] rmbaseval, sibcalc, segoffs, segdisp;
+    wire [31:0] rmbaseval, modsibcalc, segoffs, segdisp;
 
-    muxnm_tree #(.SEL_WIDTH(1), .DATA_WIDTH(32)) m1(.in({reg2[31:0],32'h00000000}), .sel(reg_or_disp), .out(rmbaseval));
-    kogeAdder #(.WIDTH(32)) add0(.SUM(sibcalc), .COUT(), .A(rmbaseval), .B(shfidx));
+    muxnm_tree #(.SEL_WIDTH(1), .DATA_WIDTH(32)) m1(.in({reg2[31:0],32'h00000000}), .sel(usereg2), .out(rmbaseval));
+    kogeAdder #(.WIDTH(32)) add0(.SUM(modsibcalc), .COUT(), .A(rmbaseval), .B(shfreg3));
     kogeAdder #(.WIDTH(32)) add1(.SUM(segdisp), .COUT(), .A(disp), .B(shfseg1));
-    muxnm_tree #(.SEL_WIDTH(1), .DATA_WIDTH(32)) m2(.in({sibcalc,reg2[31:0]}), .sel(is_sib), .out(segoffs));
+    muxnm_tree #(.SEL_WIDTH(1), .DATA_WIDTH(32)) m2(.in({modsibcalc,reg2[31:0]}), .sel(usereg3), .out(segoffs));
     kogeAdder #(.WIDTH(32)) add2(.SUM(mem_addr1), .COUT(), .A(segoffs), .B(segdisp));
     kogeAdder #(.WIDTH(32)) add3(.SUM(mem_addr2), .COUT(), .A(reg4[31:0]), .B(shfseg2));
 
@@ -112,6 +110,10 @@ module rrag (input valid_in,
     assign reg3_orig = reg_addr3;
     assign reg4_orig = reg_addr4;
 
+    assign op1_out = op1_in;
+    assign op2_out = op2_in;
+    assign op3_out = op3_in;
+    assign op4_out = op4_in;
     assign dest1_out = dest1_in;
     assign dest2_out = dest2_in;
     assign dest3_out = dest3_in;
@@ -135,7 +137,7 @@ module rrag (input valid_in,
     assign is_br_out = is_br_in;
     assign is_fp_out = is_fp_in;
     assign imm_out = imm_in;
-    assign mem1_rw_out = mem1_rw_i;
+    assign mem1_rw_out = mem1_rw_in;
     assign mem2_rw_out = mem2_rw_in;
     assign eip_out = eip_in;
     assign IE_out = IE_in;
