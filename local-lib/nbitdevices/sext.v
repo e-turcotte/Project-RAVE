720c86f7288ab83ae4748e9926534a66c859cb1f
e-turcotte <eturcotte@utexas.edu>
2024-04-27 05:42:17 -0500

    Fix Varun's SEXT

--- local-lib/nbitdevices/sext.v
+++ local-lib/nbitdevices/sext.v
@@ -2,8 +2,8 @@ module sext_8_to_32 (
     input wire [7:0] in,
     output wire [31:0] out
 );
-wire msb, msb_buffered;
-bufferH16$ b0(.out(msb_buffered), .in(msb));
+wire msb_buffered;
+bufferH16$ b0(.out(msb_buffered), .in(in[7]));
 genvar i;
 generate
     for (i = 31; i > 7; i = i - 1) begin : sext_slices
@@ -18,8 +18,8 @@ module sext_16_to_32 (
     input wire [15:0] in,
     output wire [31:0] out
 );
-wire msb, msb_buffered;
-bufferH16$ b0(.out(msb_buffered), .in(msb));
+wire msb_buffered;
+bufferH16$ b0(.out(msb_buffered), .in(in[15]));
 genvar i;
 generate
     for (i = 31; i > 15; i = i - 1) begin : sext_slices
