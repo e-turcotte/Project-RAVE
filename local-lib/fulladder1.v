a1cf92d77d93d940036209aa15a3b27913c8e807
rohanjainUTENGR <rohanjain@utexas.edu>
2024-04-29 16:37:45 -0500

    varun was complaining abt prefix d for no reason

--- local-lib/fulladder1.v
+++ local-lib/fulladder1.v
@@ -7,12 +7,12 @@ module fulladder1$(
     
     wire xor_1, nand_1, nand_2;
 
-    xor2$ (.out(xor_1), .in0(A), .in1(B));
-    xor2$ (.out(sum), .in0(xor_1), .in1(cin));
+    xor2$ x0(.out(xor_1), .in0(A), .in1(B));
+    xor2$ x1(.out(sum), .in0(xor_1), .in1(cin));
 
-    nand2$ (.out(nand_1), .in0(A), .in1(B));
-    nand2$ (.out(nand_2), .in0(xor_1), .in1(cin));
+    nand2$ n0(.out(nand_1), .in0(A), .in1(B));
+    nand2$ n1(.out(nand_2), .in0(xor_1), .in1(cin));
 
-    nand2$ (.out(cout), .in0(nand_1), .in1(nand_2));
+    nand2$ n2(.out(cout), .in0(nand_1), .in1(nand_2));
 
 endmodule
