e35da3e162ed820d6feab78fd0c01586fecdcd05
rohanjainUTENGR <rohanjain@utexas.edu>
2024-04-30 23:37:04 -0500

    add debug statements to queued latches but they are hardcoded, need to make them variable length

--- local-lib/nbitdevices/queuenm_t.v
+++ local-lib/nbitdevices/queuenm_t.v
@@ -1,6 +1,6 @@
 module TOP;
 
-    localparam CYCLE_TIME = 2.0;
+    localparam CYCLE_TIME = 2.5;
     integer k, l;
 
     localparam mlen = 16;
@@ -73,9 +73,69 @@ module TOP;
         $finish;
     end
 
+    integer cyc_cnt;
+    integer file;
+    integer latch_num;
+
     initial begin
         $dumpfile("test.fst");
         $dumpvars(5, TOP);
+
+        cyc_cnt = 0;
+        file = $fopen("queued_latch.out", "w");
+    end
+
+     wire [mlen+nlen-1:0] all_outs [qlen-1:0];
+     integer j;
+
+    always @(posedge clk) begin
+		// for (j = 0; j < qlen; j = j + 1) begin
+        // 	assign all_outs[j] = q0.outs[(latch_num+1)*(mlen+nlen)-1:latch_num*(mlen+nlen)];
+        // end
+
+        
+        $fdisplay(file, "cycle number: %d", cyc_cnt);
+        $fdisplay(file, "[===QUEUED LATCHES VALUES===]");
+
+        cyc_cnt = cyc_cnt + 1;
+        for (latch_num = 0; latch_num < qlen; latch_num = latch_num + 1) begin
+            $fdisplay(file, "LATCH %d outs: %h", latch_num, all_outs[latch_num]);
+        end
+        
+        latch_num = 0;
+        $fdisplay(file, "LATCH %d outs: %h", latch_num, q0.outs[(0+1)*(mlen+nlen)-1:0*(mlen+nlen)]);
+        latch_num = 1;
+        $fdisplay(file, "LATCH %d outs: %h", latch_num, q0.outs[(1+1)*(mlen+nlen)-1:1*(mlen+nlen)]);
+        latch_num = 2;
+        $fdisplay(file, "LATCH %d outs: %h", latch_num, q0.outs[(2+1)*(mlen+nlen)-1:2*(mlen+nlen)]);
+        latch_num = 3;
+        $fdisplay(file, "LATCH %d outs: %h", latch_num, q0.outs[(3+1)*(mlen+nlen)-1:3*(mlen+nlen)]);
+        latch_num = 4;
+        $fdisplay(file, "LATCH %d outs: %h", latch_num, q0.outs[(4+1)*(mlen+nlen)-1:4*(mlen+nlen)]);
+        latch_num = 5;
+        $fdisplay(file, "LATCH %d outs: %h", latch_num, q0.outs[(5+1)*(mlen+nlen)-1:5*(mlen+nlen)]);
+        latch_num = 6;
+        $fdisplay(file, "LATCH %d outs: %h", latch_num, q0.outs[(6+1)*(mlen+nlen)-1:6*(mlen+nlen)]);
+        latch_num = 7;
+        $fdisplay(file, "LATCH %d outs: %h", latch_num, q0.outs[(7+1)*(mlen+nlen)-1:7*(mlen+nlen)]);
+        latch_num = 8;
+        $fdisplay(file, "LATCH %d outs: %h", latch_num, q0.outs[(8+1)*(mlen+nlen)-1:8*(mlen+nlen)]);
+        latch_num = 9;
+        $fdisplay(file, "LATCH %d outs: %h", latch_num, q0.outs[(9+1)*(mlen+nlen)-1:9*(mlen+nlen)]);
+        latch_num = 10;
+        $fdisplay(file, "LATCH %d outs: %h", latch_num, q0.outs[(10+1)*(mlen+nlen)-1:10*(mlen+nlen)]);
+        latch_num = 11;
+        $fdisplay(file, "LATCH %d outs: %h", latch_num, q0.outs[(11+1)*(mlen+nlen)-1:11*(mlen+nlen)]);
+        latch_num = 12;
+        $fdisplay(file, "LATCH %d outs: %h", latch_num, q0.outs[(12+1)*(mlen+nlen)-1:12*(mlen+nlen)]);
+        latch_num = 13;
+        $fdisplay(file, "LATCH %d outs: %h", latch_num, q0.outs[(13+1)*(mlen+nlen)-1:13*(mlen+nlen)]);
+        latch_num = 14;
+        $fdisplay(file, "LATCH %d outs: %h", latch_num, q0.outs[(14+1)*(mlen+nlen)-1:14*(mlen+nlen)]);
+        latch_num = 15;
+        $fdisplay(file, "LATCH %d outs: %h", latch_num, q0.outs[(15+1)*(mlen+nlen)-1:15*(mlen+nlen)]);
+        
+        $fdisplay(file, "\n");
     end
 
 endmodule
