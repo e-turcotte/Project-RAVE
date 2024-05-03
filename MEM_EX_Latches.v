18a98843c7d4db3015ae47a35797842cfcc9c52c
rohanjainUTENGR <rohanjain@utexas.edu>
2024-05-03 03:50:06 -0500

    this was so painful please dont change the signals between MEM and EX

---
+++ MEM_EX_Latches.v
@@ -0,0 +1,151 @@
+module MEM_EX_Queued_Latches #(parameter M_WIDTH=8, N_WIDTH=8, Q_LENGTH=8) (input [M_WIDTH-1:0] m_din,
+                                                                               input [N_WIDTH-1:0] n_din,
+                                                                               input [M_WIDTH*Q_LENGTH-1:0] new_m_vector,
+                                                                               input wr, rd,
+                                                                               input [Q_LENGTH-1:0] modify_vector,
+                                                                               input clr,
+                                                                               input clk,
+                                                                               output full, empty,
+                                                                               output [M_WIDTH*Q_LENGTH-1:0] old_m_vector,
+                                                                               output [M_WIDTH+N_WIDTH-1:0] dout);
+
+      queuenm #(M_WIDTH, N_WIDTH, Q_LENGTH) q0(.m_din(m_din), .n_din(n_din), .new_m_vector(new_m_vector), .wr(wr), .rd(rd), .modify_vector(modify_vector), .clr(clr), .clk(clk), .full(full), .empty(empty), .old_m_vector(old_m_vector), .dout(dout));                                                                 
+
+    integer file;
+    initial begin
+        file = $fopen("MEM_EX_latches.out", "w");
+    end
+
+    parameter mlen = M_WIDTH;
+    parameter nlen = N_WIDTH;
+    parameter qlen = Q_LENGTH;
+
+    reg [(0+1)*(mlen+nlen)-1:0*(mlen+nlen)] all_outs[Q_LENGTH-1:0];
+    integer k, latch_num;
+
+    always @(posedge clk) begin        
+        k = 0;
+        all_outs[k] = q0.outs[(0+1)*(mlen+nlen)-1:0*(mlen+nlen)];
+        k = 1;
+        all_outs[k] = q0.outs[(1+1)*(mlen+nlen)-1:1*(mlen+nlen)];
+        k = 2;
+        all_outs[k] = q0.outs[(2+1)*(mlen+nlen)-1:2*(mlen+nlen)];
+        k = 3;
+        all_outs[k] = q0.outs[(3+1)*(mlen+nlen)-1:3*(mlen+nlen)];
+        k = 4;
+        all_outs[k] = q0.outs[(4+1)*(mlen+nlen)-1:4*(mlen+nlen)];
+        k = 5;
+        all_outs[k] = q0.outs[(5+1)*(mlen+nlen)-1:5*(mlen+nlen)];
+        k = 6;
+        all_outs[k] = q0.outs[(6+1)*(mlen+nlen)-1:6*(mlen+nlen)];
+        k = 7;
+        all_outs[k] = q0.outs[(7+1)*(mlen+nlen)-1:7*(mlen+nlen)];
+
+		$display(file, "\n=============== MEM to EX Latch Values ===============\n");
+
+        for (latch_num = 0; latch_num < qlen; latch_num = latch_num + 1) begin
+            $fdisplay(file, "\t==LATCH==: %d", latch_num);            
+            $fdisplay(file, "\t\t valid: %d", all_outs[latch_num][1068]);
+            $fdisplay(file, "\t\t eip: %d", all_outs[latch_num][1067:1036]);
+            $fdisplay(file, "\t\t IE: %d", all_outs[latch_num][1035]);
+            $fdisplay(file, "\t\t IE_type: %d", all_outs[latch_num][1034:1031]);
+            $fdisplay(file, "\t\t BR_pred_target: %d", all_outs[latch_num][1030:999]);
+            $fdisplay(file, "\t\t BR_pred_T_NT: %d", all_outs[latch_num][998]);
+            $fdisplay(file, "\t\t opsize: %d\n", all_outs[latch_num][997:996]); 
+
+            $fdisplay(file, "\t\t op1_val: %d", all_outs[latch_num][995:932]);
+            $fdisplay(file, "\t\t op2_val: %d", all_outs[latch_num][931:868]);
+            $fdisplay(file, "\t\t op3_val: %d", all_outs[latch_num][867:804]);
+            $fdisplay(file, "\t\t op4_val: %d\n", all_outs[latch_num][803:740]); 
+
+            $fdisplay(file, "\t\t op1_ptcinfo: %d", all_outs[latch_num][739:612]);
+            $fdisplay(file, "\t\t op2_ptcinfo: %d", all_outs[latch_num][611:484]);
+            $fdisplay(file, "\t\t op3_ptcinfo: %d", all_outs[latch_num][483:356]);
+            $fdisplay(file, "\t\t op4_ptcinfo: %d", all_outs[latch_num][355:228]);
+            $fdisplay(file, "\t\t dest1_addr: %d", all_outs[latch_num][227:196]);
+            $fdisplay(file, "\t\t dest2_addr: %d", all_outs[latch_num][195:164]);
+            $fdisplay(file, "\t\t dest3_addr: %d", all_outs[latch_num][163:132]);
+            $fdisplay(file, "\t\t dest4_addr: %d\n", all_outs[latch_num][131:100]); 
+
+            $fdisplay(file, "\t\t dest1_is_reg: %d", all_outs[latch_num][99]);
+            $fdisplay(file, "\t\t dest2_is_reg: %d", all_outs[latch_num][98]);
+            $fdisplay(file, "\t\t dest3_is_reg: %d", all_outs[latch_num][97]);
+            $fdisplay(file, "\t\t dest4_is_reg: %d", all_outs[latch_num][96]);
+            $fdisplay(file, "\t\t dest1_is_seg: %d", all_outs[latch_num][95]);
+            $fdisplay(file, "\t\t dest2_is_seg: %d", all_outs[latch_num][94]);
+            $fdisplay(file, "\t\t dest3_is_seg: %d", all_outs[latch_num][93]);
+            $fdisplay(file, "\t\t dest4_is_seg: %d", all_outs[latch_num][92]);
+            $fdisplay(file, "\t\t dest1_is_mem: %d", all_outs[latch_num][91]);
+            $fdisplay(file, "\t\t dest2_is_mem: %d", all_outs[latch_num][90]);
+            $fdisplay(file, "\t\t dest3_is_mem: %d", all_outs[latch_num][89]);
+            $fdisplay(file, "\t\t dest4_is_mem: %d", all_outs[latch_num][88]);
+            $fdisplay(file, "\t\t res1_ld_out: %d", all_outs[latch_num][87]);
+            $fdisplay(file, "\t\t res2_ld_out: %d", all_outs[latch_num][86]);
+            $fdisplay(file, "\t\t res3_ld_out: %d", all_outs[latch_num][85]);
+            $fdisplay(file, "\t\t res4_ld_out: %d\n", all_outs[latch_num][84]); 
+
+            $fdisplay(file, "\t\t aluk: %d", all_outs[latch_num][83:79]);
+            $fdisplay(file, "\t\t mux_adder: %d", all_outs[latch_num][78:76]);
+            $fdisplay(file, "\t\t mux_and_int: %d", all_outs[latch_num][75]);
+            $fdisplay(file, "\t\t mux_shift: %d", all_outs[latch_num][74]);
+            $fdisplay(file, "\t\t p_op: %d", all_outs[latch_num][73:37]);
+            $fdisplay(file, "\t\t fmask: %d", all_outs[latch_num][36:20]); 
+            $fdisplay(file, "\t\t conditionals: %d", all_outs[latch_num][19:18]);
+            $fdisplay(file, "\t\t is_br: %d", all_outs[latch_num][17]);
+            $fdisplay(file, "\t\t is_fp: %d", all_outs[latch_num][16]);
+            $fdisplay(file, "\t\t CS: %d", all_outs[latch_num][15:0]);
+        end
+
+        
+        $fdisplay(file, "\n");
+    end
+endmodule 
+
+//  for reference, these are the outputs from MEM coming into the latch:
+//  valid_out                   //[1068]
+//  [31:0] eip_out              //[1067:1036]
+//  IE_out                      //[1035]
+//  [3:0] IE_type_out           //[1034:1031]
+//  [31:0] BR_pred_target_out   //[1030:999]
+//  BR_pred_T_NT_out            //[998]
+
+//  [1:0] opsize_out         //[997:996]
+//  [63:0] op1_val           //[995:932]
+//  [63:0] op2_val           //[931:868]
+//  [63:0] op3_val           //[867:804]
+//  [63:0] op4_val           //[803:740]
+//  [127:0] op1_ptcinfo      //[739:612]
+//  [127:0] op2_ptcinfo      //[611:484]
+//  [127:0] op3_ptcinfo      //[483:356]
+//  [127:0] op4_ptcinfo      //[355:228]
+//  [31:0] dest1_addr        //[227:196]
+//  [31:0] dest2_addr        //[195:164]
+//  [31:0] dest3_addr        //[163:132]
+//  [31:0] dest4_addr        //[131:100]
+//  dest1_is_reg             //[99]
+//  dest2_is_reg             //[98]
+//  dest3_is_reg             //[97]
+//  dest4_is_reg             //[96]
+//  dest1_is_seg             //[95]
+//  dest2_is_seg             //[94]
+//  dest3_is_seg             //[93]
+//  dest4_is_seg             //[92]
+//  dest1_is_mem             //[91]  
+//  dest2_is_mem             //[90]
+//  dest3_is_mem             //[89]
+//  dest4_is_mem             //[88]
+//  res1_ld_out              //[87]
+//  res2_ld_out              //[86]
+//  res3_ld_out              //[85]
+//  res4_ld_out              //[84]
+
+//  [4:0] aluk_out,           //[83:79]  
+//  [2:0] mux_adder_out       //[78:76]
+//  mux_and_int_out           //[75]
+//  mux_shift_out             //[74]
+//  [36:0] p_op_out           //[73:37]  
+//  [16:0] fmask_out          //[36:20]
+//  [1:0] conditionals_out    //[19:18]
+//  is_br_out,                //[17]
+//  is_fp_out                 //[16]
+//  [15:0] CS_out             //[15:0]
---
+++ RrAg_MEM_Latch.v
@@ -0,0 +1,208 @@
+
+module RrAg_MEM_latch (
+        input ld, clr,
+        input clk,
+
+        input valid_RrAg_MEM_latch_in, stall_RrAg_MEM_latch_in,
+        input [1:0] opsize_RrAg_MEM_latch_in,
+        input [31:0] mem_addr1_RrAg_MEM_latch_in, mem_addr2_RrAg_MEM_latch_in, mem_addr1_end_RrAg_MEM_latch_in, mem_addr2_end_RrAg_MEM_latch_in,
+        input [63:0] reg1_RrAg_MEM_latch_in, reg2_RrAg_MEM_latch_in, reg3_RrAg_MEM_latch_in, reg4_RrAg_MEM_latch_in,
+        input [127:0] ptc_r1_RrAg_MEM_latch_in, ptc_r2_RrAg_MEM_latch_in, ptc_r3_RrAg_MEM_latch_in, ptc_r4_RrAg_MEM_latch_in,
+        input [2:0] reg1_orig_RrAg_MEM_latch_in, reg2_orig_RrAg_MEM_latch_in, reg3_orig_RrAg_MEM_latch_in, reg4_orig_RrAg_MEM_latch_in,
+        input [15:0] seg1_RrAg_MEM_latch_in, seg2_RrAg_MEM_latch_in, seg3_RrAg_MEM_latch_in, seg4_RrAg_MEM_latch_in,
+        input [31:0] ptc_s1_RrAg_MEM_latch_in, ptc_s2_RrAg_MEM_latch_in, ptc_s3_RrAg_MEM_latch_in, ptc_s4_RrAg_MEM_latch_in,
+        input [2:0] seg1_orig_RrAg_MEM_latch_in, seg2_orig_RrAg_MEM_latch_in, seg3_orig_RrAg_MEM_latch_in, seg4_orig_RrAg_MEM_latch_in,
+        input [6:0] inst_ptcid_RrAg_MEM_latch_in,
+        input [12:0] op1_out_RrAg_MEM_latch_in, op2_out_RrAg_MEM_latch_in, op3_out_RrAg_MEM_latch_in, op4_out_RrAg_MEM_latch_in,
+        input [12:0] dest1_out_RrAg_MEM_latch_in, dest2_out_RrAg_MEM_latch_in, dest3_out_RrAg_MEM_latch_in, dest4_out_RrAg_MEM_latch_in,
+        input res1_ld_out_RrAg_MEM_latch_in, res2_ld_out_RrAg_MEM_latch_in, res3_ld_out_RrAg_MEM_latch_in, res4_ld_out_RrAg_MEM_latch_in,
+        input [31:0] rep_num_RrAg_MEM_latch_in,
+        input [4:0] aluk_out_RrAg_MEM_latch_in,
+        input [2:0] mux_adder_out_RrAg_MEM_latch_in,
+        input mux_and_int_out_RrAg_MEM_latch_in, mux_shift_out_RrAg_MEM_latch_in,
+        input [36:0] p_op_out_RrAg_MEM_latch_in,
+        input [17:0] fmask_out_RrAg_MEM_latch_in,
+        input [15:0]  CS_out_RrAg_MEM_latch_in,
+        input [1:0] conditionals_out_RrAg_MEM_latch_in,
+        input is_br_out_RrAg_MEM_latch_in, is_fp_out_RrAg_MEM_latch_in,
+        input [47:0] imm_out_RrAg_MEM_latch_in,
+        input [1:0] mem1_rw_out_RrAg_MEM_latch_in, mem2_rw_out_RrAg_MEM_latch_in,
+        input [31:0] eip_out_RrAg_MEM_latch_in,
+        input IE_out_RrAg_MEM_latch_in,
+        input [3:0] IE_type_out_RrAg_MEM_latch_in,
+        input [31:0] BR_pred_target_out_RrAg_MEM_latch_in,
+        input BR_pred_T_NT_out_RrAg_MEM_latch_in,
+
+        output valid_RrAg_MEM_latch_out, stall_RrAg_MEM_latch_out,
+        output [1:0] opsize_RrAg_MEM_latch_out,
+        output [31:0] mem_addr1_RrAg_MEM_latch_out, mem_addr2_RrAg_MEM_latch_out, mem_addr1_end_RrAg_MEM_latch_out, mem_addr2_end_RrAg_MEM_latch_out,
+        output [63:0] reg1_RrAg_MEM_latch_out, reg2_RrAg_MEM_latch_out, reg3_RrAg_MEM_latch_out, reg4_RrAg_MEM_latch_out,
+        output [127:0] ptc_r1_RrAg_MEM_latch_out, ptc_r2_RrAg_MEM_latch_out, ptc_r3_RrAg_MEM_latch_out, ptc_r4_RrAg_MEM_latch_out,
+        output [2:0] reg1_orig_RrAg_MEM_latch_out, reg2_orig_RrAg_MEM_latch_out, reg3_orig_RrAg_MEM_latch_out, reg4_orig_RrAg_MEM_latch_out,
+        output [15:0] seg1_RrAg_MEM_latch_out, seg2_RrAg_MEM_latch_out, seg3_RrAg_MEM_latch_out, seg4_RrAg_MEM_latch_out,
+        output [31:0] ptc_s1_RrAg_MEM_latch_out, ptc_s2_RrAg_MEM_latch_out, ptc_s3_RrAg_MEM_latch_out, ptc_s4_RrAg_MEM_latch_out,
+        output [2:0] seg1_orig_RrAg_MEM_latch_out, seg2_orig_RrAg_MEM_latch_out, seg3_orig_RrAg_MEM_latch_out, seg4_orig_RrAg_MEM_latch_out,
+        output [6:0] inst_ptcid_RrAg_MEM_latch_out,
+        output [12:0] op1_RrAg_MEM_latch_out, op2_RrAg_MEM_latch_out, op3_RrAg_MEM_latch_out, op4_RrAg_MEM_latch_out,
+        output [12:0] dest1_RrAg_MEM_latch_out, dest2_RrAg_MEM_latch_out, dest3_RrAg_MEM_latch_out, dest4_RrAg_MEM_latch_out,
+        output res1_ld_out_RrAg_MEM_latch_out, res2_ld_out_RrAg_MEM_latch_out, res3_ld_out_RrAg_MEM_latch_out, res4_ld_out_RrAg_MEM_latch_out,
+        output [31:0] rep_num_RrAg_MEM_latch_out,
+        output [4:0] aluk_RrAg_MEM_latch_out,
+        output [2:0] mux_adder_RrAg_MEM_latch_out,
+        output mux_and_int_RrAg_MEM_latch_out, mux_shift_RrAg_MEM_latch_out,
+        output [36:0] p_op_RrAg_MEM_latch_out,
+        output [17:0] fmask_RrAg_MEM_latch_out,
+        output [15:0]  CS_out_RrAg_MEM_latch_out,
+        output [1:0] conditionals_RrAg_MEM_latch_out,
+        output is_br_RrAg_MEM_latch_out, is_fp_RrAg_MEM_latch_out,
+        output [47:0] imm_RrAg_MEM_latch_out,
+        output [1:0] mem1_rw_RrAg_MEM_latch_out, mem2_rw_RrAg_MEM_latch_out,
+        output [31:0] eip_RrAg_MEM_latch_out,
+        output IE_RrAg_MEM_latch_out,
+        output [3:0] IE_type_RrAg_MEM_latch_out,
+        output [31:0] BR_pred_target_RrAg_MEM_latch_out,
+        output BR_pred_T_NT_RrAg_MEM_latch_out
+
+        );
+    
+    integer file;
+    initial begin
+        file = $fopen("RrAg_MEM_latches.out", "w");
+    end
+
+    regn #(.WIDTH(1)) r1 (.din(valid_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(valid_RrAg_MEM_latch_out));
+    regn #(.WIDTH(1)) r2 (.din(stall_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(stall_RrAg_MEM_latch_out));
+    regn #(.WIDTH(2)) r3 (.din(opsize_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(opsize_RrAg_MEM_latch_out));
+    regn #(.WIDTH(32)) r4 (.din(mem_addr1_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(mem_addr1_RrAg_MEM_latch_out));
+    regn #(.WIDTH(32)) r5 (.din(mem_addr2_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(mem_addr2_RrAg_MEM_latch_out));
+    regn #(.WIDTH(32)) r6 (.din(mem_addr1_end_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(mem_addr1_end_RrAg_MEM_latch_out));
+    regn #(.WIDTH(32)) r7 (.din(mem_addr2_end_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(mem_addr2_end_RrAg_MEM_latch_out));
+    regn #(.WIDTH(64)) r8 (.din(reg1_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(reg1_RrAg_MEM_latch_out));
+    regn #(.WIDTH(64)) r9 (.din(reg2_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(reg2_RrAg_MEM_latch_out));
+    regn #(.WIDTH(64)) r10 (.din(reg3_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(reg3_RrAg_MEM_latch_out));
+    regn #(.WIDTH(64)) r11 (.din(reg4_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(reg4_RrAg_MEM_latch_out));
+    regn #(.WIDTH(128)) r12 (.din(ptc_r1_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(ptc_r1_RrAg_MEM_latch_out));
+    regn #(.WIDTH(128)) r13 (.din(ptc_r2_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(ptc_r2_RrAg_MEM_latch_out));
+    regn #(.WIDTH(128)) r14 (.din(ptc_r3_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(ptc_r3_RrAg_MEM_latch_out));
+    regn #(.WIDTH(128)) r15 (.din(ptc_r4_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(ptc_r4_RrAg_MEM_latch_out));
+    regn #(.WIDTH(3)) r16 (.din(reg1_orig_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(reg1_orig_RrAg_MEM_latch_out));
+    regn #(.WIDTH(3)) r17 (.din(reg2_orig_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(reg2_orig_RrAg_MEM_latch_out));
+    regn #(.WIDTH(3)) r18 (.din(reg3_orig_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(reg3_orig_RrAg_MEM_latch_out));
+    regn #(.WIDTH(3)) r19 (.din(reg4_orig_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(reg4_orig_RrAg_MEM_latch_out));
+    regn #(.WIDTH(16)) r20 (.din(seg1_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(seg1_RrAg_MEM_latch_out));
+    regn #(.WIDTH(16)) r21 (.din(seg2_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(seg2_RrAg_MEM_latch_out));
+    regn #(.WIDTH(16)) r22 (.din(seg3_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(seg3_RrAg_MEM_latch_out));
+    regn #(.WIDTH(16)) r23 (.din(seg4_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(seg4_RrAg_MEM_latch_out));
+    regn #(.WIDTH(32)) r24 (.din(ptc_s1_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(ptc_s1_RrAg_MEM_latch_out));
+    regn #(.WIDTH(32)) r25 (.din(ptc_s2_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(ptc_s2_RrAg_MEM_latch_out));
+    regn #(.WIDTH(32)) r26 (.din(ptc_s3_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(ptc_s3_RrAg_MEM_latch_out));
+    regn #(.WIDTH(32)) r27 (.din(ptc_s4_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(ptc_s4_RrAg_MEM_latch_out));
+    regn #(.WIDTH(3)) r28 (.din(seg1_orig_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(seg1_orig_RrAg_MEM_latch_out));
+    regn #(.WIDTH(3)) r29 (.din(seg2_orig_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(seg2_orig_RrAg_MEM_latch_out));
+    regn #(.WIDTH(3)) r30 (.din(seg3_orig_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(seg3_orig_RrAg_MEM_latch_out));
+    regn #(.WIDTH(3)) r31 (.din(seg4_orig_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(seg4_orig_RrAg_MEM_latch_out));
+    regn #(.WIDTH(7)) r32 (.din(inst_ptcid_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(inst_ptcid_RrAg_MEM_latch_out));
+    regn #(.WIDTH(13)) r33 (.din(op1_out_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(op1_RrAg_MEM_latch_out));
+    regn #(.WIDTH(13)) r34 (.din(op2_out_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(op2_RrAg_MEM_latch_out));
+    regn #(.WIDTH(13)) r35 (.din(op3_out_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(op3_RrAg_MEM_latch_out));
+    regn #(.WIDTH(13)) r36 (.din(op4_out_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(op4_RrAg_MEM_latch_out));
+    regn #(.WIDTH(13)) r37 (.din(dest1_out_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(dest1_RrAg_MEM_latch_out));
+    regn #(.WIDTH(13)) r38 (.din(dest2_out_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(dest2_RrAg_MEM_latch_out));
+    regn #(.WIDTH(13)) r39 (.din(dest3_out_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(dest3_RrAg_MEM_latch_out));
+    regn #(.WIDTH(13)) r40 (.din(dest4_out_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(dest4_RrAg_MEM_latch_out));
+    regn #(.WIDTH(1)) r41 (.din(res1_ld_out_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(res1_ld_out_RrAg_MEM_latch_out));
+    regn #(.WIDTH(1)) r42 (.din(res2_ld_out_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(res2_ld_out_RrAg_MEM_latch_out));
+    regn #(.WIDTH(1)) r43 (.din(res3_ld_out_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(res3_ld_out_RrAg_MEM_latch_out));
+    regn #(.WIDTH(1)) r44 (.din(res4_ld_out_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(res4_ld_out_RrAg_MEM_latch_out));
+    regn #(.WIDTH(32)) r45 (.din(rep_num_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(rep_num_RrAg_MEM_latch_out));
+    regn #(.WIDTH(5)) r46 (.din(aluk_out_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(aluk_RrAg_MEM_latch_out));
+    regn #(.WIDTH(3)) r47 (.din(mux_adder_out_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(mux_adder_RrAg_MEM_latch_out));
+    regn #(.WIDTH(1)) r48 (.din(mux_and_int_out_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(mux_and_int_RrAg_MEM_latch_out));
+    regn #(.WIDTH(1)) r49 (.din(mux_shift_out_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(mux_shift_RrAg_MEM_latch_out));
+    regn #(.WIDTH(37)) r50 (.din(p_op_out_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(p_op_RrAg_MEM_latch_out));
+    regn #(.WIDTH(18)) r51 (.din(fmask_out_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(fmask_RrAg_MEM_latch_out));
+    regn #(.WIDTH(16)) r52 (.din(CS_out_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(CS_out_RrAg_MEM_latch_out));
+    regn #(.WIDTH(2)) r63 (.din(conditionals_out_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(conditionals_RrAg_MEM_latch_out));
+    regn #(.WIDTH(1)) r53 (.din(is_br_out_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(is_br_RrAg_MEM_latch_out));
+    regn #(.WIDTH(1)) r54 (.din(is_fp_out_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(is_fp_RrAg_MEM_latch_out));
+    regn #(.WIDTH(48)) r55 (.din(imm_out_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(imm_RrAg_MEM_latch_out));
+    regn #(.WIDTH(2)) r56 (.din(mem1_rw_out_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(mem1_rw_RrAg_MEM_latch_out));
+    regn #(.WIDTH(2)) r57 (.din(mem2_rw_out_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(mem2_rw_RrAg_MEM_latch_out));
+    regn #(.WIDTH(32)) r58 (.din(eip_out_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(eip_RrAg_MEM_latch_out));
+    regn #(.WIDTH(1)) r59 (.din(IE_out_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(IE_RrAg_MEM_latch_out));
+    regn #(.WIDTH(4)) r60 (.din(IE_type_out_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(IE_type_RrAg_MEM_latch_out));
+    regn #(.WIDTH(32)) r61 (.din(BR_pred_target_out_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(BR_pred_target_RrAg_MEM_latch_out));
+    regn #(.WIDTH(1)) r62 (.din(BR_pred_T_NT_out_RrAg_MEM_latch_in), .ld(ld), .clr(clr), .clk(clk), .dout(BR_pred_T_NT_RrAg_MEM_latch_out));
+
+    always @(posedge clk) begin
+		$fdisplay(file, "\n=============== RrAg to MEM Latch Values ===============\n");
+ 
+        $fdisplay(file, "\t\t valid: 0x%h", valid_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t stall: 0x%h", stall_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t opsize: 0x%h", opsize_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t mem_addr1: 0x%h", mem_addr1_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t mem_addr2: 0x%h", mem_addr2_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t mem_addr1_end: 0x%h", mem_addr1_end_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t mem_addr2_end: 0x%h", mem_addr2_end_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t reg1: 0x%h", reg1_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t reg2: 0x%h", reg2_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t reg3: 0x%h", reg3_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t reg4: 0x%h", reg4_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t ptc_r1: 0x%h", ptc_r1_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t ptc_r2: 0x%h", ptc_r2_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t ptc_r3: 0x%h", ptc_r3_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t ptc_r4: 0x%h", ptc_r4_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t reg1_orig: 0x%h", reg1_orig_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t reg2_orig: 0x%h", reg2_orig_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t reg3_orig: 0x%h", reg3_orig_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t reg4_orig: 0x%h", reg4_orig_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t seg1: 0x%h", seg1_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t seg2: 0x%h", seg2_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t seg3: 0x%h", seg3_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t seg4: 0x%h", seg4_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t ptc_s1: 0x%h", ptc_s1_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t ptc_s2: 0x%h", ptc_s2_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t ptc_s3: 0x%h", ptc_s3_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t ptc_s4: 0x%h", ptc_s4_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t seg1_orig: 0x%h", seg1_orig_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t seg2_orig: 0x%h", seg2_orig_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t seg3_orig: 0x%h", seg3_orig_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t seg4_orig: 0x%h", seg4_orig_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t inst_ptcid: 0x%h", inst_ptcid_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t op1: 0x%h", op1_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t op2: 0x%h", op2_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t op3: 0x%h", op3_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t op4: 0x%h", op4_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t dest1: 0x%h", dest1_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t dest2: 0x%h", dest2_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t dest3: 0x%h", dest3_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t dest4: 0x%h", dest4_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t res1_ld_out: 0x%h", res1_ld_out_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t res2_ld_out: 0x%h", res2_ld_out_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t res3_ld_out: 0x%h", res3_ld_out_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t res4_ld_out: 0x%h", res4_ld_out_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t rep_num: 0x%h", rep_num_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t aluk: 0x%h", aluk_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t mux_adder: 0x%h", mux_adder_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t mux_and_int: 0x%h", mux_and_int_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t mux_shift: 0x%h", mux_shift_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t p_op: 0x%h", p_op_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t fmask: 0x%h", fmask_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t CS: 0x%h", CS_out_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t conditionals: 0x%h", conditionals_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t is_br: 0x%h", is_br_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t is_fp: 0x%h", is_fp_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t imm: 0x%h", imm_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t mem1_rw: 0x%h", mem1_rw_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t mem2_rw: 0x%h", mem2_rw_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t eip: 0x%h", eip_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t IE: 0x%h", IE_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t IE_type: 0x%h", IE_type_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t BR_pred_target: 0x%h", BR_pred_target_RrAg_MEM_latch_out);
+        $fdisplay(file, "\t\t BR_pred_T_NT: 0x%h", BR_pred_T_NT_RrAg_MEM_latch_out);
+        
+		
+		$display("\n=================================================\n");    
+	end
+
+endmodule
