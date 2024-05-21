320958334cb4f2fe74b6aa53537bda1b20b97488
Varun Arumugam <varun.bala@utexas.edu>
2024-05-20 22:01:21 -0500

    silly goose

--- local-lib/nbitdevices/regn.v
+++ local-lib/nbitdevices/regn.v
@@ -32,7 +32,7 @@ module regn_with_set #(parameter WIDTH=16) (input [WIDTH-1:0] din,
 
 endmodule
 
-module regn_with_set_lsb #(parameter PARITY=0, WIDTH=16) (input [WIDTH-1:0] din,
+module regn_with_set_lsb_FIP #(parameter PARITY=0, WIDTH=16) (input [WIDTH-1:0] din,
                                    input ld, clr,
                                    input clk,
                                    output [WIDTH-1:0] dout);
@@ -41,15 +41,19 @@ module regn_with_set_lsb #(parameter PARITY=0, WIDTH=16) (input [WIDTH-1:0] din,
  
     genvar i;
     generate
+        for (i = 0; i < 4; i = i + 1) begin : reg_slices
+            mux2$ g0(.outb(datatold[i]), .in0(dout[i]), .in1(din[i]), .s0(ld));
+            dff$ g1(.clk(clk), .d(datatold[i]), .q(dout[i]), .qbar(), .r(clr), .s(1'b1));
+        end
         if(PARITY == 1) begin
-            mux2$ g0wkdnfvkds(.outb(datatold[0]), .in0(dout[0]), .in1(din[0]), .s0(ld));
-            dff$ gsdflnsdl1(.clk(clk), .d(datatold[0]), .q(dout[0]), .qbar(), .r(1'b1), .s(clr));
+            mux2$ g0wkdnfvkds(.outb(datatold[4]), .in0(dout[4]), .in1(din[4]), .s0(ld));
+            dff$ gsdflnsdl1(.clk(clk), .d(datatold[4]), .q(dout[4]), .qbar(), .r(1'b1), .s(clr));
         end
         else begin
-            mux2$ gsdmfnls0(.outb(datatold[0]), .in0(dout[0]), .in1(din[0]), .s0(ld));
-            dff$ gsdfksj1(.clk(clk), .d(datatold[0]), .q(dout[0]), .qbar(), .r(clr), .s(1'b1));
+            mux2$ gsdmfnls0(.outb(datatold[4]), .in0(dout[4]), .in1(din[4]), .s0(ld));
+            dff$ gsdfksj1(.clk(clk), .d(datatold[4]), .q(dout[4]), .qbar(), .r(clr), .s(1'b1));
         end
-        for (i = 1; i < WIDTH; i = i + 1) begin : reg_slices
+        for (i = 5; i < WIDTH; i = i + 1) begin : sadasf
             mux2$ g0(.outb(datatold[i]), .in0(dout[i]), .in1(din[i]), .s0(ld));
             dff$ g1(.clk(clk), .d(datatold[i]), .q(dout[i]), .qbar(), .r(clr), .s(1'b1));
         end
