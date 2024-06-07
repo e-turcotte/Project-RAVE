10d539a84a79f2b4cb883e5a55fb9c6e9d71d9ba
Asher Nederveld <jnederveld@mario.ece.utexas.edu>
2024-06-06 23:35:14 -0500

    :(

--- processor/M/cache/SW_R_SWP.v
+++ processor/M/cache/SW_R_SWP.v
@@ -24,22 +24,22 @@ module SW_R_SWP(
 );
 assign PTC_ID_in_sw = PTC_ID_in;
 assign PTC_ID_in_r = PTC_ID_in;
-nor2$ swselalt(swSel, M1_RW[1], M2_RW[1]);
-nor2$ rselalt(rSel, M1_RW[0], M2_RW[0]);
+nor2$ swselalt(swSel, M1_RW[0], M2_RW[0]);
+nor2$ rselalt(rSel, M1_RW[1], M2_RW[1]);
 nor2$ sels(Sel, M1_RW[1], M1_RW[0]);
-muxnm_tristate #(3, 32) mx0({M2, M1, 32'd0}, {M2_RW[1],M1_RW[1],swSel},address_in_sw);
-muxnm_tristate #(3, 32) mx1({M2, M1, 32'd0}, {M2_RW[0],M1_RW[0],rSel},address_in_r);
+muxnm_tristate #(3, 32) mx0({M2, M1, 32'd0}, {M2_RW[0],M1_RW[0],swSel},address_in_sw);
+muxnm_tristate #(3, 32) mx1({M2, M1, 32'd0}, {M2_RW[1],M1_RW[1],rSel},address_in_r);
 
-muxnm_tristate #(3, 2) mx2({2'b10, 2'b01, 2'd0}, {M1_RW[1],M1_RW[0], Sel},{sw_is_m1, r_is_m1});
+muxnm_tristate #(3, 2) mx2({2'b10, 2'b01, 2'd0}, {M1_RW[0],M1_RW[1], Sel},{sw_is_m1, r_is_m1});
 
 assign sizeOVR_sw = sizeOvr;
 assign sizeOVR_r = sizeOvr;
 mux2n #(2) mxsws(size_in_sw, size_in, 2'b11, sizeOvr);
 mux2n #(2) mxrs(size_in_r, size_in, 2'b11, sizeOvr);
 
-nor2$ vsw(vsw_1, M1_RW[1], M2_RW[1]);
+nor2$ vsw(vsw_1, M1_RW[0], M2_RW[0]);
 
-nor2$ vr(vr_1, M1_RW[0], M2_RW[0]);
+nor2$ vr(vr_1, M1_RW[1], M2_RW[1]);
 inv1$ vn(valid_rsw_n, valid_rsw);
 
 nor2$ vsw1(valid_in_sw, vsw_1, valid_rsw_n);
