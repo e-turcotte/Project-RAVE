3ab504c1c3756a1086c29f8eb0eee79c66ab00cf
Varun Arumugam <varun.bala@utexas.edu>
2024-06-22 18:33:09 -0500

    asher makes me sad sometimes :( -eddie

--- processor/RrAg/rrag_TOP.v
+++ processor/RrAg/rrag_TOP.v
@@ -88,7 +88,12 @@ module rrag (input valid_in,
     genvar i;
     generate
         for (i = 0; i < 13; i = i + 1) begin : sized_ld_dest_slices
-            or4$ g1(.out(collated_dest_vector[i]), .in0(dest1_in[i]), .in1(dest2_in[i]), .in2(dest3_in[i]), .in3(dest4_in[i]));
+            wire [3:0] guarded_dest;
+            and2$ g123(.out(guarded_dest), .in0(dest1_in[i]), .in1(res1_ld_in));
+            and2$ g456(.out(guarded_dest), .in0(dest2_in[i]), .in1(res2_ld_in));
+            and2$ g789(.out(guarded_dest), .in0(dest3_in[i]), .in1(res3_ld_in));
+            and2$ gabc(.out(guarded_dest), .in0(dest4_in[i]), .in1(res4_ld_in));
+            or4$ g1(.out(collated_dest_vector[i]), .in0(guarded_dest[3]), .in1(guarded_dest[2]), .in2(guarded_dest[1]), .in3(guarded_dest[0]));
         end
     endgenerate
 
