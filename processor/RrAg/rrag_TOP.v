81ec0be539f099d3c13ebcc0fc407600001796f8
e-turcotte <eturcotte@utexas.edu>
2024-06-25 23:34:29 -0500

    rrag rep stall logic fix pt2

--- processor/RrAg/rrag_TOP.v
+++ processor/RrAg/rrag_TOP.v
@@ -185,6 +185,6 @@ module rrag (input valid_in,
     or2$ g6(.out(modrmsib_ptc), .in0(modrm_ptc), .in1(sib_ptc));
     and2$ g7(.out(mem1_stall), .in0(mem1_use), .in1(modrmsib_ptc));
     and2$ g8(.out(mem2_stall), .in0(mem2_or_rep_use), .in1(regformem4ptc));
-    and2$ g9(.out(rep_stall), .in0(is_rep_in), .in1(regformem3ptc));
+    and2$ g11(.out(rep_stall), .in0(is_rep_in), .in1(regformem3ptc));
     or3$ g10(.out(stall), .in0(fwd_stall), .in1(mem1_stall), .in2(mem2_stall), .in3(rep_stall));
 endmodule
