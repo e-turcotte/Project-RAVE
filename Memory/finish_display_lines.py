35ead020e2868fcadca01a54bb403404ddd34dcd
e-turcotte <eturcotte@utexas.edu>
2024-07-07 01:52:03 -0500

    Add pmem_final.out pt5

--- Memory/finish_display_lines.py
+++ Memory/finish_display_lines.py
@@ -7,7 +7,7 @@ def main():
             code += '$fdisplay(file, \"[0x%h]:\\t%04h %04h %04h %04h\\t%04h %04h %04h %04h\\t%04h %04h %04h %04h\\t%04h %04h %04h %04h\", {4\'b'+('{:04b}'.format(chip))+',5\'b'+('{:05b}'.format(cloch))+',2\'b00,4\'b0000}, '
             for bank in range(0,4):
                 for dram in range(0,4):
-                    code += 'banks['+str(bank)+'].bnk.bank_slices['+str(chip)+'].dram.cells['+str(dram)+'].sram.mem['+str(32*(cloch+1)-1)+':'+str(32*cloch)+']'
+                    code += 'banks['+str(bank)+'].bnk.bank_slices['+str(chip)+'].dram.cells['+str(dram)+'].sram.mem['+str(cloch)+']'
                     code += ');' if dram == 3 and bank == 3 else ', '
             print(code)
             code = ''
