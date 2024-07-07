c912d986176e2e57107e34ebfc416192f238d412
e-turcotte <eturcotte@utexas.edu>
2024-07-07 02:17:04 -0500

    Add pmem_final.out pt7

--- Memory/finish_display_lines.py
+++ Memory/finish_display_lines.py
@@ -4,7 +4,7 @@ def main():
     code = ''
     for chip in range(0,16):
         for cloch in range(0,32):
-            code += '$fdisplay(file, \"[0x%h]:    %0h %0h %0h %0h    %0h %0h %0h %0h    %0h %0h %0h %0h    %0h %0h %0h %0h\", {4\'b'+('{:04b}'.format(chip))+',5\'b'+('{:05b}'.format(cloch))+',2\'b00,4\'b0000}, '
+            code += '$fdisplay(file, \"[0x%h]:    %h %h %h %h    %h %h %h %h    %h %h %h %h    %h %h %h %h\", {4\'b'+('{:04b}'.format(chip))+',5\'b'+('{:05b}'.format(cloch))+',2\'b00,4\'b0000}, '
             for bank in range(0,4):
                 for dram in range(0,4):
                     code += 'banks['+str(bank)+'].bnk.bank_slices['+str(chip)+'].dram.cells['+str(dram)+'].sram.mem['+str(cloch)+']'
