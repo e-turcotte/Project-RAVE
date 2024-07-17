9bc4f46abca6007c4b29ecedf98844297b690fb6
Varun Arumugam <varun.bala@utexas.edu>
2024-07-16 23:36:50 -0500

    fixed saturated counter

--- local-lib/sat_cntr2.v
+++ local-lib/sat_cntr2.v
@@ -9,15 +9,26 @@ module sat_cntr2(
 );
 
     wire [1:0] NS, S, notS;
+    wire not_in;
+    inv$ i0(not_in, in);
 
+    wire clk_temp;
     and2$ a0(clk_temp, clk, enable);
 
-    and3$ a1(f1, notS[0], in);
-    and3$ a2(f2, S[1], S[0], in);
-    or3$ o0(NS[0], f1, f2);
+    wire f1, f2, f3, f4, f5;
+    and3$ a1(f1, notS[1], notS[0], in); //00 + 1 -> 0[1]
+    and3$ a2(f2, notS[1], S[0], not_in);//01 + 0 -> 0[1]
+    and3$ a3(f3, S[1], notS[0], not_in);//10 + 0 -> 0[1]
+    and3$ a4(f4, S[1], notS[0], in);//10 + 1 -> 1[1]
+    and3$ a5(f5, S[1], S[0], in);//11 + 1 -> 1[1]
+    orn #(5) o0(.in({f1, f2, f3, f4, f5}), .out(NS[0]));
 
-    and3$ a3(f3, S[0], in);
-    assign NS[1] = f3;
+    wire f6, f7, f8;
+    and3$ a6(f6, notS[1], S[0], in);//01 + 1 -> [1]0
+    and3$ a7(f7, S[1], notS[1], in);//10 + 1 -> [1]1
+    and2$ a8(f8, S[1], S[1]);//11 + 0 -> [1]0 || 11 + 1 -> [1]1
+    orn #(3) o1(.in({f6, f7, f8}), .out(NS[1]));
+
 
     dff$ s1(clk_temp, NS[1], S[1], notS[1], rst_n, set_n);
     dff$ s2(clk_temp, NS[0], S[0], notS[0], rst_n, set_n);
