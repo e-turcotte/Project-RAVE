9e8a571cd366bfad04aaf890cdf04eab20ac1b33
e-turcotte <eturcotte@utexas.edu>
2024-07-16 22:41:43 -0500

    Fix sat counter nextstate logic

--- local-lib/sat_cntr2.v
+++ local-lib/sat_cntr2.v
@@ -12,15 +12,12 @@ module sat_cntr2(
 
     and2$ a0(clk_temp, clk, enable);
 
-    and2$ a1(f1 ,S[1], notS[0]);
-    and3$ a2(f2, notS[1], notS[0], in);
-    and3$ a3(f3, S[1], S[0], in);
-    or3$ o0(NS[0], f1, f2, f3);
+    and3$ a1(f1, notS[0], in);
+    and3$ a2(f2, S[1], S[0], in);
+    or3$ o0(NS[0], f1, f2);
 
-    and2$ a4(f4, S[1], S[0]);
-    and3$ a5(f5, S[1], notS[0], in);
-    and3$ a6(f6, notS[1], S[0], in);
-    or3$ o1(NS[1], f4, f5, f6);
+    and3$ a3(f3, S[0], in);
+    assign NS[1] = f3;
 
     dff$ s1(clk_temp, NS[1], S[1], notS[1], rst_n, set_n);
     dff$ s2(clk_temp, NS[0], S[0], notS[0], rst_n, set_n);
