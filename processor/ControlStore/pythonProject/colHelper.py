47d0dbd4ca4b5b532cd54db7cb86671d042b718a
Asher Nederveld <jnederveld@mario.ece.utexas.edu>
2024-07-20 22:09:25 -0500

    Fixed issue with RETfar not hitting

--- processor/ControlStore/pythonProject/colHelper.py
+++ processor/ControlStore/pythonProject/colHelper.py
@@ -1448,7 +1448,7 @@ def helperOP(row, op, asm):
     elif (opH == "8'hC3" or opH == "8'hC2" ):
         print("COLUMN NEAR HIT ")
         RETnear(row, op, asm)
-    elif (opcode == "RET"):
+    elif (opH == "8'hCB" or opH == "8'hCA"):
         print("column ret hit")
         RETfar(row, op, asm)
     elif (opcode == "SAL"):
@@ -1472,7 +1472,8 @@ def helperOP(row, op, asm):
     elif (opcode == "JMP" or opcode == "JNE" or opcode == "JNBE"):
         JMPnear(row, op, asm)
     else:
-        print(opcode)
+        print( str(opcode) + "failure" + str(opH) + "  " + str(asm))
+
     #
 
 
